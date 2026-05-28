bc2cc35260b1ef2e0d79f9d37a1b0c2ff92c24e2
isaiah777-sys <fakoredesemiloore@gmail.com>
2026-05-28 12:22:14 +0100

    leetcode

--- leetcode/LongestCommonPrefix.go
+++ leetcode/LongestCommonPrefix.go
@@ -1,33 +1,19 @@
 package leetcode
 
+import "strings"
+
 func longestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
 		return ""
 	}
+
 	prefix := strs[0]
-	for _, str := range strs[1:] {
-		for len(prefix) > 0 && str[:len(prefix)] != prefix {
+
+	for _, s := range strs[1:] {
+		for len(prefix) > 0 && !strings.HasPrefix(s, prefix) {
 			prefix = prefix[:len(prefix)-1]
 		}
-		if len(prefix) == 0 {
-			return ""
-		}
 	}
-	return prefix
-}
 
-// This is not the solution, but it's just a program to print the duplicates in a list: not this list btw.
-func printDuplicates(nums []int) []int {
-	duplicates := []int{}
-	for _, number := range nums {
-		if number > len(nums) {
-			continue
-		}
-		if nums[number-1] < 0 {
-			duplicates = append(duplicates, number)
-		} else {
-			nums[number-1] = -nums[number-1]
-		}
-	}
-	return duplicates
+	return prefix
 }
